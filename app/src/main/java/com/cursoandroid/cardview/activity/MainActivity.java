30f09e902f4b748cc7c40e7d47f71ac2e60c3e62
LUCAS ANDRÉ <lucas_andre_spfc@hotmail.com>
2020-04-05 22:35:23 -0300

    Commit CardView - part 3: Finalizando...

--- app/src/main/java/com/cursoandroid/cardview/activity/MainActivity.java
+++ app/src/main/java/com/cursoandroid/cardview/activity/MainActivity.java
@@ -1,22 +1,27 @@
 package com.cursoandroid.cardview.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import com.cursoandroid.cardview.R;
 import com.cursoandroid.cardview.adapter.PostagemAdapter;
 import com.cursoandroid.cardview.model.Postagem;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerPostagem;
-    private List<Postagem> postagens;
+    private List<Postagem> postagens = new ArrayList<>();
 
+    @SuppressLint("WrongConstant")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,8 +30,9 @@ public class MainActivity extends AppCompatActivity {
         recyclerPostagem = findViewById(R.id.recyclerPostagem);
 
         // Define layout
-
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager( this);
+        //LinearLayoutManager layoutManager = new LinearLayoutManager( this);
+        //layoutManager.setOrientation(LinearLayout.HORIZONTAL);
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(this, 2);
         recyclerPostagem.setLayoutManager(layoutManager);
 
         // Define adapter
